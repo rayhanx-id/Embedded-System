93fea21aae3f27742fef3afafe6aa412fc963712
rayhanx-id <75841306+rayhanx-id@users.noreply.github.com>
2023-10-12 22:39:42 +0700

    Update deteksi_ketinggian_banjir.ino

--- ESP-32 Board/deteksi_ketinggian_banjir.ino
+++ ESP-32 Board/deteksi_ketinggian_banjir.ino
@@ -4,6 +4,7 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 const int trigPin = 2; // Pin trigger sensor ultrasonik
 const int echoPin = 4; // Pin echo sensor ultrasonik
+const int buzzer = 13; // Pin buzzer
 
 void setup() {
   Serial.begin(9600);
@@ -12,6 +13,7 @@ void setup() {
   lcd.backlight();
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
@@ -31,19 +33,28 @@ void loop() {
     lcd.print("Banjir Terdeteksi");
     lcd.setCursor(0,1);
     lcd.print("Rendah!!!");
+    noTone(buzzer); // Turn off the buzzer
+
   } else if (distance >= 30 && distance < 60) {
     lcd.setCursor(0,0);
     lcd.print("Banjir Terdeteksi");
     lcd.setCursor(0,1);
     lcd.print("Normal!!!");
+    noTone(buzzer); // Turn off the buzzer
+
   } else if (distance >= 60) {
     lcd.setCursor(0,0);
     lcd.print("Banjir Terdeteksi");
     lcd.setCursor(0,1);
     lcd.print("Tinggi!!!");
+    tone(buzzer, 500);
+    delay(1000);
+    tone(buzzer, 650);
+    delay(1000); 
+    
   } else {
     lcd.print("Tidak Ada Banjir");
   }
   
-  delay(3000);
+  delay(1000);
 }
